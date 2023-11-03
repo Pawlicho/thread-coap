bd7cdba77bba17042c1a7431b3ae38a4e403ad45
Pawel Tymoczko <pawel.tymoczko@gmail.com>
2023-11-03 17:36:00 +0100

    Added app resources PoC to the server & Included client dummy testing app

--- coap-server/my_coap_server.py
+++ coap-server/my_coap_server.py
@@ -8,7 +8,92 @@ class HelloWorldResource(resource.Resource):
     async def render_get(self, request):
         print("Received GET request")
         payload = "Hello there!!!\n".encode('ascii')
-        return aiocoap.Message(payload=payload)    
+        return aiocoap.Message(payload=payload)
+
+class CurrTempResource(resource.Resource):
+    def __init__(self):
+        super().__init__()
+        self.temp = 21.5
+
+    def encodeContent(self):
+        retVal = "{:.2f}".format(self.temp)
+        self.encoded = retVal.encode('ascii')
+
+    def decodeContent(self, payload):
+        decoded = payload.decode('ascii')
+        self.temp = float(decoded)
+
+    async def render_get(self, request):
+        self.encodeContent()
+        return aiocoap.Message(payload=self.encoded)
+    
+    async def render_put(self, request):
+        print('PUT payload: %s' % request.payload)
+        self.decodeContent(request.payload)
+        self.encodeContent()
+        return aiocoap.Message(code=aiocoap.CHANGED, payload=self.encoded)
+    
+class SetTempResource(resource.Resource):
+    def __init__(self):
+        super().__init__()
+        self.temp = 20.0
+
+    def encodeContent(self):
+        retVal = "{:.2f}".format(self.temp)
+        self.encoded = retVal.encode('ascii')
+
+    def decodeContent(self, payload):
+        decoded = payload.decode('ascii')
+        self.temp = float(decoded)
+
+    async def render_get(self, request):
+        self.encodeContent()
+        return aiocoap.Message(payload=self.encoded)
+    
+    async def render_put(self, request):
+        print('PUT payload: %s' % request.payload)
+        self.decodeContent(request.payload)
+        self.encodeContent()
+        return aiocoap.Message(code=aiocoap.CHANGED, payload=self.encoded)
+    
+class IsRoomOccupiedResource(resource.Resource):
+    def __init__(self):
+        super().__init__()
+        self.is_occupied = 0
+
+    def encodeContent(self):
+        self.encoded = str(self.is_occupied).encode('ascii')
+
+    def decodeContent(self, payload):
+        decoded = payload.decode('ascii')
+        self.is_occupied = int(decoded)
+
+    async def render_get(self, request):
+        self.encodeContent()
+        return aiocoap.Message(payload=self.encoded)
+    
+    async def render_put(self, request):
+        print('PUT payload: %s' % request.payload)
+        self.decodeContent(request.payload)
+        self.encodeContent()
+        return aiocoap.Message(code=aiocoap.CHANGED, payload=self.encoded)
+
+class AdjustTempResource(resource.Resource):
+    def __init__(self):
+        super().__init__()
+        self.adjustment = 0.0
+
+    def encodeContent(self):
+        retVal = "{:.2f}".format(self.adjustment)
+        self.encoded = retVal.encode('ascii')
+
+    def decodeContent(self, payload):
+        decoded = payload.decode('ascii')
+        self.adjustment = float(decoded)
+
+    async def render_get(self, request):
+        self.encodeContent()
+        return aiocoap.Message(payload=self.encoded)
 
 async def main():
 
@@ -18,6 +103,10 @@ async def main():
 
     root = resource.Site()
     root.add_resource(['hello_world'], HelloWorldResource())
+    root.add_resource(['curr_temp'], CurrTempResource())
+    root.add_resource(['set_temp'], SetTempResource())
+    root.add_resource(['is_room_occupied'], IsRoomOccupiedResource())
+    root.add_resource(['adjust_temp'], AdjustTempResource())
 
     await aiocoap.Context.create_server_context(root, bind=(host, port))
 
