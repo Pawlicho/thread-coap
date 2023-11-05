7b5eb2207db8e57d8892ef9aa0ff4196840a4d1b
Pawel Tymoczko <pawel.tymoczko@gmail.com>
2023-11-05 20:56:37 +0100

    WEB GUI - PoC

--- coap-server/test_web.py
+++ coap-server/test_web.py
@@ -1,24 +1,67 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, render_template, request
 import sqlite3
+
+from create_db import CreateTables
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+db_name = 'thread_coap_database.db'
 
-@app.route('/save_parameter', methods=['POST'])
-def save_parameter():
-    name = request.form['name']
-    value = request.form['value']
-
-    conn = sqlite3.connect('mydatabase.db')
+def insert_data(temperature1, temperature2):
+    conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO parameters (name, value) VALUES (?, ?)', (name, value))
+    cursor.execute("INSERT INTO WEB_MANAGER_LOGS (set_temperature_1, set_temperature_2) VALUES (?, ?)",
+                   (temperature1, temperature2))
     conn.commit()
     conn.close()
 
-    return redirect(url_for('index'))
+def get_web_manager_last_update():
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM WEB_MANAGER_LOGS ORDER BY time DESC LIMIT 1")
+    last_update = cursor.fetchone()
+    conn.close()
+    return last_update
+
+def get_client_1_logs():
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM CLIENT_1_LOGS")
+    logs = cursor.fetchall()
+    conn.close()
+    return logs
+
+def get_client_2_logs():
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM CLIENT_2_LOGS")
+    logs = cursor.fetchall()
+    conn.close()
+    return logs
+
+@app.route('/')
+def index():
+    last_update = get_web_manager_last_update()
+    return render_template('index.html', last_update=last_update)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    temperature1 = request.form['temperature1']
+    temperature2 = request.form['temperature2']
+    insert_data(temperature1, temperature2)
+    last_update = get_web_manager_last_update()
+    return render_template('index.html', last_update=last_update)
+
+@app.route('/logs/client1')
+def logs_client1():
+    logs_data = get_client_1_logs()
+    return render_template('logs_client1.html', logs_data=logs_data)
+
+@app.route('/logs/client2')
+def logs_client2():
+    logs_data = get_client_2_logs()
+    return render_template('logs_client2.html', logs_data=logs_data)
 
 if __name__ == '__main__':
-    app.run()
+    CreateTables(db_name)
+    app.run(debug=True)
