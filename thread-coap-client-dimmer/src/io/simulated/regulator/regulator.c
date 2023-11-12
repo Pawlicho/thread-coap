d86cc4eabf2eac0638eae3cbb349896e057826a8
Pawel Tymoczko <pawel.tymoczko@gmail.com>
2023-11-12 19:14:19 +0100

    Added matching regulator for dimmer

--- thread-coap-client-dimmer/src/io/simulated/regulator/regulator.c
+++ thread-coap-client-dimmer/src/io/simulated/regulator/regulator.c
@@ -12,11 +12,36 @@ static struct k_timer regulate_timer;
 
 static struct k_work regulate_work;
 
-static const float regulator_resolution = 0.2f;
+static const float regulator_resolution = 10.0f;
 
 static void regulate_work_cb(struct k_work *work)
 {
-    // LOG_INF("Current Illuminance: %.2f\n", dimmer.current_illuminance);
+    /* Update temperature if correction needed */
+    /* Maximum single correction is bounded by regulator_resolution */
+    /* e.g. 0.2 degrees */
+
+    /* If temperature can be corrected in single tick */
+
+    if (fabs(dimmer.correction) >= regulator_resolution)
+    {
+        /* If temperature must be increased */
+        if (dimmer.correction > 0)
+        {
+            /* Increase temperature */
+            dimmer.current_illuminance += regulator_resolution;
+            /* Update needed correction */
+            dimmer.correction -= regulator_resolution;
+        }
+        /* If temperature must be decreased */
+        else
+        {
+            /* Decrease temperature */
+            dimmer.current_illuminance -= regulator_resolution;
+            /* Update needed correction */
+            dimmer.correction += regulator_resolution;
+        }
+    }
+    LOG_INF("Current Illuminance: %.2f\n", dimmer.current_illuminance);
 }
 
 static void regulate_timer_cb(struct k_timer *timer)
