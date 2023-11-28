f4e902d45d97bcc66f20fc75743bbbe959da4368
Pawel Tymoczko <pawel.tymoczko@gmail.com>
2023-11-28 20:49:30 +0100

    New application architecture - server side, no logs, tested heater

--- coap-server/coap_server.py
+++ coap-server/coap_server.py
@@ -4,7 +4,7 @@ import asyncio
 import aiocoap
 import aiocoap.resource as resource
 
-from coordinator import periodic_task, ServerSetParameters, ServerCurrentParameters, calc_correction_temp, calc_correction_illuminance
+from coordinator import periodic_task, ServerSetParameters, ServerCurrentParameters, calc_heater_reg_param, calc_dimmer_reg_param
 from logs_manager import LogsManager
 from database.create_db import CreateTables
 
@@ -13,92 +13,145 @@ server_set_params = ServerSetParameters()
 server_cur_params = ServerCurrentParameters()
 logs_manager = LogsManager(db_path)
 
-class HeaterResource(resource.Resource):
+class TemperatureResource(resource.Resource):
     def __init__(self):
         super().__init__()
         self.temperature = 21.5
 
-    def encodeContent(self, value):
-        retVal = "{:.2f}".format(value)
-        self.encoded = retVal.encode('ascii')
+    def encode_content(self, value):
+        ret_val = "{:.2f}".format(value)
+        self.encoded = ret_val.encode('ascii')
 
-    def decodeContent(self, payload):
+    def decode_content(self, payload):
         decoded = payload.decode('ascii')
         decoded = decoded.strip('\x00')
         self.temperature = float(decoded)
 
     async def render_get(self, request):
-        print("\nReceived heater.GET.Req\n")
-        self.encodeContent(self.temperature)
-        print("\Responding with heater.GET.Rsp\n")
+        # print("\nReceived temperature.GET.Req\n")
+        self.encode_content(self.temperature)
+        # print("\nResponding with temperature.GET.Rsp\n")
         return aiocoap.Message(payload=self.encoded)
     
     async def render_put(self, request):
-        print("\nReceived heater.PUT.Req")
+        # print("\nReceived temperature.PUT.Req")
 
         # Decode received request's payload from ascii, convert to temp format
-        self.decodeContent(request.payload)
-        print("New current_temp value on the server side: " + str(self.temperature))
+        self.decode_content(request.payload)
+        # print("\nNew current_temp value on the server side: " + str(self.temperature))
 
         # Update current temperature value from client req
         server_cur_params.UpdateHeater(self.temperature)
 
-        # Calculate temperature correction to send in resp
-        correction = calc_correction_temp(server_set_params.GetTemperature(), server_cur_params.GetTemperature())
-
-        # Encode response payload in ascii
-        self.encodeContent(correction)
-
-        # Log request
-        logs_manager.AddHeaterLog(self.temperature, correction, request.remote.hostinfo)
-        
+        # Log request TODO
+        # logs_manager.AddHeaterLog(self.temperature, correction, request.remote.hostinfo)
 
         # Send the response to client
-        print("Responding with heater.PUT.Rsp\n")
-        return aiocoap.Message(code=aiocoap.CHANGED, payload=self.encoded)
+        # Echo received payload
+        # print("\nResponding with temperature.PUT.Rsp\n")
+        return aiocoap.Message(code=aiocoap.CHANGED, payload=request.payload)
     
-class DimmerResource(resource.Resource):
+class HeaterRegulationResource(resource.Resource):
+    def __init__(self):
+        super().__init__()
+        self.current_power = 20
+
+    def encode_content(self, value):
+        ret_val = "{:.1f}".format(value)
+        self.encoded = ret_val.encode('ascii')
+
+    def decode_content(self, payload):
+        decoded = payload.decode('ascii')
+        decoded = decoded.strip('\x00')
+        self.current_power = float(decoded)
+
+    async def render_get(self, request):
+        print("\nReceived heater_regulation.GET.Req\n")
+
+        # Decode received power, acquire current power
+        self.decode_content(request.payload)
+
+        # Calculate regulation parameter
+        regulation_parameter = calc_heater_reg_param(server_set_params.GetTemperature(), server_cur_params.GetTemperature(), self.current_power)
+
+        print("\nCurrent temperature: " + str(server_cur_params.GetTemperature()))
+        print("\nSet temperature: " + str(server_set_params.GetTemperature()))
+        print("\nCurrent power: " + str(self.current_power))
+
+        # Encode regulation parameter
+        self.encode_content(regulation_parameter)
+
+        print("\nResponding with heater_regulation.GET.Rsp\n")
+
+        return aiocoap.Message(payload=self.encoded)
+    
+class IlluminanceResource(resource.Resource):
     def __init__(self):
         super().__init__()
         self.illuminance = 300
 
-    def encodeContent(self, value):
-        retVal = "{:.1f}".format(value)
-        self.encoded = retVal.encode('ascii')
+    def encode_content(self, value):
+        ret_val = "{:.1f}".format(value)
+        self.encoded = ret_val.encode('ascii')
 
-    def decodeContent(self, payload):
+    def decode_content(self, payload):
         decoded = payload.decode('ascii')
         decoded = decoded.strip('\x00')
         self.illuminance = float(decoded)
 
     async def render_get(self, request):
         print("\nReceived dimmer.GET.Req\n")
-        self.encodeContent(self.illuminance)
-        print("\Responding with dimmer.GET.Rsp\n")
+        self.encode_content(self.illuminance)
+        print("\nResponding with dimmer.GET.Rsp\n")
         return aiocoap.Message(payload=self.encoded)
     
     async def render_put(self, request):
         print("\nReceived dimmer.PUT.Req")
 
         # Decode received request's payload from ascii, convert to temp format
-        self.decodeContent(request.payload)
-        print("New current_illuminance value on the server side: " + str(self.illuminance))
+        self.decode_content(request.payload)
+        print("\nNew current_illuminance value on the server side: " + str(self.illuminance))
 
         # Update current illuminance value from client req
         server_cur_params.UpdateDimmer(self.illuminance)
 
-        # Calculate illuminance correction to send in resp
-        correction = calc_correction_illuminance(server_set_params.GetIlluminance(), server_cur_params.GetIlluminance())
-
-        # Encode response payload in ascii
-        self.encodeContent(correction)
-
-        # Log request
-        logs_manager.AddDimmerLog(self.illuminance, correction, request.remote.hostinfo)
+        # Log request TODO: MODIFY
+        # logs_manager.AddDimmerLog(self.illuminance, correction, request.remote.hostinfo)
         
         # Send the response to client
-        print("Responding with dimmer.PUT.Rsp\n")
-        return aiocoap.Message(code=aiocoap.CHANGED, payload=self.encoded)
+        # Echo request payload
+        print("\nResponding with dimmer.PUT.Rsp\n")
+        return aiocoap.Message(code=aiocoap.CHANGED, payload=request.payload)
+
+class DimmerRegulationResource(resource.Resource):
+    def __init__(self):
+        super().__init__()
+        self.current_power = 20
+
+    def encode_content(self, value):
+        ret_val = "{:d}".format(value)
+        self.encoded = ret_val.encode('ascii')
+
+    def decode_content(self, payload):
+        decoded = payload.decode('ascii')
+        decoded = decoded.strip('\x00')
+        self.current_power = float(decoded)
+
+    async def render_get(self, request):
+        print("\nReceived dimmer_regulation.GET.Req\n")
+
+        # Decode received power, acquire current power
+        self.decode_content(request.payload)
+
+        # Calculate regulation parameter
+        regulation_parameter = calc_dimmer_reg_param(server_set_params.GetIlluminance, server_cur_params.GetIlluminanc, self.current_power)
+
+        # Encode regulation parameter
+        self.encode_content(regulation_parameter)
+
+        print("\nResponding with dimmer_regulation.GET.Rsp\n")
+
+        return aiocoap.Message(payload=self.encoded)
 
 async def main():
 
@@ -111,8 +164,10 @@ async def main():
 
     # Set up resources on server side
     root = resource.Site()
-    root.add_resource(['heater'], HeaterResource())
-    root.add_resource(['dimmer'], DimmerResource())
+    root.add_resource(['temperature'], TemperatureResource())
+    root.add_resource(['illuminance'], IlluminanceResource())
+    root.add_resource(['heater_regulation'], HeaterRegulationResource())
+    root.add_resource(['dimmer_regulation'], DimmerRegulationResource())
 
     # Routine for database set parameters pulling
     periodic_task_coroutine = asyncio.create_task(periodic_task(server_set_params, logs_manager))
--- coap-server/coordinator.py
+++ coap-server/coordinator.py
@@ -39,13 +39,42 @@ class ServerCurrentParameters:
     def GetIlluminance(self):
         return self.set_illuminance
 
-def calc_correction_temp(set_val, curr_val):
-    correction = set_val - curr_val
-    return round(correction, 2)
+def calc_heater_reg_param(set_val, curr_val, curr_power):
+    
+    delta_t = set_val - curr_val
 
-def calc_correction_illuminance(set_val, curr_val):
-    correction = set_val - curr_val
-    return round(correction, 1)
+    if delta_t < 0:
+        reg_param = 0.0
+    elif delta_t >= 10:
+        reg_param = 100.0
+    elif delta_t < 10 and delta_t >= 9:
+        reg_param = 90.0
+    elif delta_t < 9 and delta_t >= 8:
+        reg_param = 80.0
+    elif delta_t < 7 and delta_t >= 6:
+        reg_param = 70.0
+    elif delta_t < 6 and delta_t >= 5:
+        reg_param = 60.0
+    elif delta_t < 5 and delta_t >= 5:
+        reg_param = 50.0
+    elif delta_t < 4 and delta_t >= 4:
+        reg_param = 40.0
+    elif delta_t < 3 and delta_t >= 2:
+        reg_param = 30.0
+    elif delta_t < 2 and delta_t >= 1:
+        reg_param = 20.0
+    elif delta_t < 1 and delta_t >= 0.5:
+        reg_param = 10.0
+    else:
+        reg_param = 0.0
+
+    print("Returning: " + str(reg_param))
+    # return round(reg_param, 2)
+    return reg_param
+
+def calc_dimmer_reg_param(set_val, curr_val, curr_power):
+    reg_param = 10
+    return reg_param
 
 async def pull_data(state):
     async with aiosqlite.connect(db_path) as db:
